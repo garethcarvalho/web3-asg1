fc548c3e9abcc570bfdfafee7cf37b72f549e033
Gareth Carvalho <garethcarva@gmail.com>
2024-02-12 17:31:38 -0700

    created circuit roots, working on constructors

--- src/controllers/f1-controller.js
+++ src/controllers/f1-controller.js
@@ -8,7 +8,7 @@ function handleSeasons(server) {
             .select();
 
         if (error) {
-            jsonMsg(res, "Error fetching from 'seasons' table");
+            jsonMsg(res, "Error fetching from 'seasons' table", error);
             return;
         }
         
@@ -16,10 +16,128 @@ function handleSeasons(server) {
     });
 }
 
+function handleCircuits(server) {
+    server.get('/api/circuits', async (req, res) => {
+        const { data, error } = await sb
+            .from('circuits')
+            .select();
+        
+        if (error) {
+            jsonMsg(res, "Error fetching from 'circuits' table", error);
+            return;
+        }
 
+        res.json(data);
+    });
+
+    server.get('/api/circuits/:ref', async (req, res) => {
+        const circuitRef = req.params.ref;
+
+        if (typeof(circuitRef) === 'undefined') {
+            jsonMsg(res, 'Enter a valid circuit reference.');
+            return;
+        }
+
+        const { data, error } = await sb
+            .from('circuits')
+            .select()
+            .eq('circuitRef', circuitRef);
+        
+        if (error) {
+            jsonMsg(res, "Error fetching from 'circuits' table", error);
+            return;
+        }
+        
+        res.json(data);
+    });
+
+    server.get('/api/circuits/season/:year', async (req, res) => {
+        const year = req.params.year;
+        
+        if (typeof(year) === 'undefined') {
+            jsonMsg(res, 'Enter a valid year.');
+            return;
+        }
+
+        const { data, error } = await sb
+            // .from('circuits')
+            // .select(`*, races!inner(year, round)`)
+            // .eq('races.year', year);
+            .from('races')
+            .select('year, round, circuits!inner(*)')
+            .eq('year', year)
+            .order('round', { ascending: true });
+        
+        if (error) {
+            jsonMsg(res, "Error fetching from 'circuits' table", error);
+            return;
+        }
+        
+        res.json(data);
+    });
+}
+
+function handleConstructors(server) {
+    server.get('/api/constructors', async (req, res) => {
+        const { data, error } = await sb
+            .from('constructors')
+            .select();
+
+        if (error) {
+            jsonMsg(res, "Error fetching from 'constructors' table", error);
+            return;
+        }
+        
+        res.json(data);
+    });
+
+    server.get('/api/constructors/:ref', async (req, res) => {
+        const constructorRef = req.params.ref;
+
+        if (typeof(constructorRef) === 'undefined') {
+            jsonMsg(res, 'Enter a valid constructor reference.');
+            return;
+        }
+
+        const { data, error } = await sb
+            .from('constructors')
+            .select()
+            .eq('constructorRef', constructorRef);
+        
+        if (error) {
+            jsonMsg(res, "Error fetching from 'constructors' table", error);
+            return;
+        }
+        
+        res.json(data);
+    });
+
+    server.get('/api/constructors/season/:year', async (req, res) => {
+        const year = req.params.year;
+        
+        if (typeof(year) === 'undefined') {
+            jsonMsg(res, 'Enter a valid year.');
+            return;
+        }
+
+        const { data, error } = await sb
+            .from('constructor_standings')
+            .select('constructors(constructorId), races()')
+            .eq('races.year', year);
+            
+        if (error) {
+            jsonMsg(res, "Error fetching from 'constructors' table", error);
+            return;
+        }
+        
+        res.json(data);
+    });
+}
 
 module.exports = {
-    handleSeasons
+    handleSeasons,
+    handleCircuits,
+    handleConstructors
 };
 
 /**
@@ -27,6 +145,8 @@ module.exports = {
  * @param {*} res - The object representing the response. Must have a `json()` function.
  * @param {string} msg - The message you want to send.
  */
-function jsonMsg(res, msg) {
+function jsonMsg(res, msg, error = null) {
     res.json({message: msg});
+    if (error)
+        console.log(error);
 }
--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ server.use(express.json());
 
 const controller = require('./controllers/f1-controller.js');
 controller.handleSeasons(server);
+controller.handleCircuits(server);
+controller.handleConstructors(server);
 
 const port = 8080;
 server.listen(port, () => {
@@ -12,5 +14,7 @@ server.listen(port, () => {
     
     Sample Routes: 
     http://localhost:${port}/api/seasons
+    http://localhost:${port}/api/circuits/albert_park
+    http://localhost:${port}/api/circuits
     `);
 });
